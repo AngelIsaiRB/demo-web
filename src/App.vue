9713c004f0333599600520ea4cc918587405611d
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-06-15 17:55:06 -0500

    nuevo header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="bg-trasparent  w-screen h-16 flex items-center justify-center ">
-    <div class="flex items-center animate__animated animate__backInLeft text-white font-bold
-      mx-8 bg-gray-100 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-40 h-full px-5
-       cursor-pointer text-2xl">
+  <div class="absolute z-50 bg-trasparent  w-screen h-8 md:h-16 flex items-center justify-between ">
+    <div class="text-white text-sm md:text-3xl md:font-bold ml-6 animate__animated animate__fadeInDown">CBIMULTIASSETS</div>
+    <div class="flex items-center md:w-auto  justify-end md:justify-between">
+      <div class="flex items-center animate__animated animate__backInLeft text-white font-bold
+      mx-1 md:mx-8 bg-gray-100 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-40 h-full px-5
+       cursor-pointer text-xs md:text-2xl">
       <router-link  to="/">Home</router-link> 
     </div>
     <div class="flex items-center animate__animated animate__backInRight text-white font-bold
-      mx-8 bg-gray-100 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-40 h-full px-5
-       cursor-pointer text-2xl">
+      mx-1 md:mx-8 bg-gray-100 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-40 h-full px-5
+       cursor-pointer text-xs md:text-2xl">
       <router-link  to="/about">Otro</router-link>
+    </div>
     </div>
   </div>
   <router-view/>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-
+    <threeanimationinicialVue/>
   </div>
 </template>
 
 <script>
-
+import threeanimationinicialVue from '../components/threeanimationinicial.vue'
 export default {
   name: 'Home',
   components: {
+    threeanimationinicialVue,
   }
 }
 </script>
