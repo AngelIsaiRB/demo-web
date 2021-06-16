8c8f950703b848ff08c2a5a3f8016a586693cc07
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-06-15 20:07:06 -0500

    first

---
+++ src/components/Principal3dComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-screen h-screen absolute top-0">
+      <div class="w-full h-full" id="globe">
+
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <threeanimationinicialVue/>
+    <Principal3dComponentVue/>
   </div>
 </template>
 
 <script>
+import Principal3dComponentVue from '../components/Principal3dComponent.vue'
 import threeanimationinicialVue from '../components/threeanimationinicial.vue'
 export default {
   name: 'Home',
   components: {
     threeanimationinicialVue,
+    Principal3dComponentVue
   }
 }
 </script>
