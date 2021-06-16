0120780453158941ade8fde5ad8b37572f3e0059
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-06-15 21:03:10 -0500

    steap 3

--- src/components/TitlesComponet.vue
+++ src/components/TitlesComponet.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="h-screen flex items-center">
-      <div>
-          <p class="text-black text-2xl md:text-4xl">REVOLUCIONAMOS </p>
-          <p class="text-black text-2xl md:text-4xl">INVERSIONES </p>
+      <div class="ml-2 md:ml-40">
+          <p class="text-black font-bold text-2xl md:text-8xl">REVOLUCIONAMOS </p>          
+          <p class="invisible md:visible text-black font-bold text-2xl md:text-8xl textanimated">INVERSIONES</p>
+          <p class="visible md:invisible -mt-6 text-black font-bold text-2xl md:text-8xl textanimatedphone">INVERSIONES</p>
+          
       </div>
   </div>
 </template>
 
 <script>
+import gsap from "gsap";
 export default {
-
+mounted () {
+    gsap.to(".textanimated", {duration: 2, x: 300});
+    gsap.to(".textanimatedphone", {duration: 2, x: 75});
+},
 }
 </script>
 
